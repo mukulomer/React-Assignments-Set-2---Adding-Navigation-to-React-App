407b537f0244dab47dd2f4151ccaf652d44ccc65
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-11-26 12:36:42 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 import Home from "./Home";
 import About from "./About";
-//import Location from "./LocationDisplay";
+// import Location from "./LocationDisplay";
 import { useLocation, Link, Route, Switch } from "react-router-dom";
 
 function withMyHook(Component) {
@@ -13,18 +13,25 @@ function withMyHook(Component) {
 }
 
 class App extends Component {
+  that = this;
+
   render() {
     const location = this.props.myHookValue;
     console.log(location);
     return (
       <>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
         <div id="main">
           <p data-testid="location-display"> {location.pathname}</p>
-
-          <a href="/">Home</a>
-          <a href="/about">About</a>
+          {/* <Location /> */}
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">Dashboard</Link>
+          </nav>
+          {location.pathname === "/" || location.pathname === "/about"
+            ? ""
+            : "Not Match"}
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
 
           {/* Do not remove the main div */}
         </div>
--- src/components/LocationDisplay.jsx
+++ src/components/LocationDisplay.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import useLocation from "react-router-dom";
 
-export default function Location() {
-  //let location = useLocation();
+export default function Location({ that }) {
+  let location = useLocation();
 
   return (
     <>
